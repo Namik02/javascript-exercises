9506b91254154fe71c3cd5055035074d1a544afd
Namik02 <Hasannamik02@outlook.com>
2022-07-12 12:00:23 -0700

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let l = str.length - 1
+    let a = str.split('')
+    let newStr = ''
+    for (let i = l; i > -1; i--) {
+       newStr = newStr + a[i]
+    }
+    return newStr
 
 };
 
