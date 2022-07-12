8b958bdf885359d4a0c74c2f2c97e7679d9455ec
Namik02 <Hasannamik02@outlook.com>
2022-07-12 15:15:32 -0700

    04 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let ans = 0
 
+    let start = null
+    let end = null
+    if (num1 < num2) {
+        start = num1
+        end = num2
+    } else {
+        start = num2
+        end = num1
+    }
+
+    if (start < 0 || typeof start !== 'number' || typeof end !== 'number') {
+        return "ERROR"
+    } else {
+        for (let i = start; i <= end; i++) {
+            ans = ans + i
+            
+        }
+        return ans
+    }
+    
 };
 
 // Do not edit below this line
