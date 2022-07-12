69a51cb97d48ada965125dce9b7bf3f0a3468c76
Namik02 <Hasannamik02@outlook.com>
2022-07-12 12:53:55 -0700

    04 progress-test 1 working

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,num) {
+    const newArr = []
+    let p = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== num) {
+            newArr[p] = arr[i]
+            p++
+        }
+        
+    }
+    return newArr
 };
 
 // Do not edit below this line
