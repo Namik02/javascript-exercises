76c5c31cac9732126adb0ab41caea1f566644591
Namik02 <Hasannamik02@outlook.com>
2022-07-13 03:38:27 -0700

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(num) {
+  newNum = (num-32)*0.5556
+  newNum = parseFloat(newNum.toFixed(1))
+  return newNum
 
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  newNum = (num*1.8)+32
+  newNum = parseFloat(newNum.toFixed(1))
+  return newNum
 };
 
 // Do not edit below this line
