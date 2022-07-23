3ef1bf5c701878fbdfd9b2d28fa8a384f8e02e47
Namik02 <Hasannamik02@outlook.com>
2022-07-23 04:46:35 -0700

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,20 +8,27 @@ const subtract = function(num1, num2) {
   return result
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0
+	array.forEach(num => result+=num);
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1
+	array.forEach(num => result*=num);
+  return result
+}
+const power = function(num, power) {
+	return num ** power
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1
+	for (let i = 1; i <= num; i++) {
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
