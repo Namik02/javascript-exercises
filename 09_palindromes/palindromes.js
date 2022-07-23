1437f070038f24ac0d5291434cda4e1ba7553af0
Namik02 <Hasannamik02@outlook.com>
2022-07-23 09:57:39 -0700

    08 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    newString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    newString = [...newString];
+    newString = newString.filter(i => i !==" ")
+    newString = newString.
+    newString = newString.map(i => i.toLowerCase())
+    
+     return newString.join('') === newString.reverse().join('')
 };
 
 // Do not edit below this line
