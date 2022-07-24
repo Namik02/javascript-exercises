54df11bad0610b2d6f01a1bc4b31b5c789eee1ce
Namik02 <Hasannamik02@outlook.com>
2022-07-24 04:57:49 -0700

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    array.sort(function(a,b) {
+            const currentDate =  new Date().getFullYear()
+        const lastGuy = ((a.yearOfDeath !== undefined) ? a.yearOfDeath : currentDate) - a.yearOfBirth
+        const nextGuy = ((b.yearOfDeath !== undefined) ? b.yearOfDeath : currentDate) - b.yearOfBirth
+        return lastGuy > nextGuy ? -1 : 1
 
+    })
+    return array[0]
 };
 
 // Do not edit below this line
